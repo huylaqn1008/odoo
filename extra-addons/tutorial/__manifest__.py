01000e068381d37e2881712899ef37ddfcff3607
Mola <huylaqn1008@gmail.com>
2023-04-17 17:04:48 +0700

    continue ODOO

--- extra-addons/tutorial/__manifest__.py
+++ extra-addons/tutorial/__manifest__.py
@@ -2,16 +2,14 @@
 {
     'name': "tutorial",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': "Tutorial Huy",
 
     'description': """
-        Long description of module's purpose
+        Huy description of module's purpose
     """,
 
     'author': "Lê Tấn Huy",
-    'website': "https://www.yourcompany.com",
+    'website': "https://github.com/huylaqn1008/odoo",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -24,9 +22,10 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+        'views/menu_view.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- extra-addons/tutorial/models/models.py
+++ extra-addons/tutorial/models/models.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields
 
 
-# class tutorial(models.Model):
-#     _name = 'tutorial.tutorial'
-#     _description = 'tutorial.tutorial'
+class Game(models.Model):
+    _name = 'tutorial.game'
+    _description = 'Game model of Tutorial App'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char("Name", required=True)
+    image = fields.Binary("Cover")
+
+    released_date = fields.Date()
+    publisher_ids = fields.Many2many(
+        "res.partner", relation="publisher_rel", string="Publishers" 
+    )
+    developer_ids = fields.Many2many(
+        "res.partner", relation="developer_rel", string="Developers"
+    )
